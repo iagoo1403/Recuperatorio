0d18893856ef6a371c4edcd8f767feee55a0bc6b
iagoo1403 <santycaffaro7@gmail.com>
2023-11-21 14:13:38 -0300

    .

--- recuperatorio/src/test/java/parcial_backend/recuperatorio/RecuperatorioApplicationTests.java
+++ recuperatorio/src/test/java/parcial_backend/recuperatorio/RecuperatorioApplicationTests.java
@@ -2,12 +2,25 @@ package parcial_backend.recuperatorio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RecuperatorioApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	private final TestRestTemplate restTemplate = new TestRestTemplate();
+
 	@Test
-	void contextLoads() {
+	void testGetAllArtists() {
+		String url = "http://localhost:" + port + "/api/artist";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
 	}
+
 
 }
