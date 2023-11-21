5de9ac566d015bd7e13f6f4ebca12b48d8a8e4be
iagoo1403 <santycaffaro7@gmail.com>
2023-11-21 14:48:50 -0300

    ,

--- recuperatorio/src/test/java/parcial_backend/recuperatorio/RecuperatorioApplicationTests.java
+++ recuperatorio/src/test/java/parcial_backend/recuperatorio/RecuperatorioApplicationTests.java
@@ -21,6 +21,62 @@ class RecuperatorioApplicationTests {
 
 
 	}
+	@Test
+	void testGetAllAlbums() {
+		String url = "http://localhost:" + port + "/api/album";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
+	}
+	@Test
+	void testGetAllCustomers() {
+		String url = "http://localhost:" + port + "/api/customers";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
+	}
+	@Test
+	void testGetAllGenre() {
+		String url = "http://localhost:" + port + "/api/genre";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
+	}
+	@Test
+	void testGetAllInvoice_item() {
+		String url = "http://localhost:" + port + "/api/invoice_item";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
+	}
+	@Test
+	void testGetAllInvoice() {
+		String url = "http://localhost:" + port + "/api/invoice";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
+	}
+	@Test
+	void testGetAllPlaylist_track() {
+		String url = "http://localhost:" + port + "/api/playlist_track";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
+	}
+	@Test
+	void testGetAllPlaylist() {
+		String url = "http://localhost:" + port + "/api/playlist";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
+	}
+	@Test
+	void testGetAllTrack() {
+		String url = "http://localhost:" + port + "/api/track";
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+
+	}
 
 
 }
